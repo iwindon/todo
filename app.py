5ea007916fd4766b46e78f2075949d08ccbcc4e7
iwindon <ivan.windon@icloud.com>
2024-06-13 11:24:42 -0400

    added edit feature

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 todos = []
 
 while True:
-    todo = input("Enter 'add' or 'show' or 'exit': ")
+    todo = input("Enter 'add', 'show', 'edit' or 'exit': ")
+    todo = todo.strip().lower()
 
-    if todo == "add":
-        todo = input("Enter a to-do: ")       
-        todos.append(todo)
-    elif todo == "show":
-        print("To-do list:")
-        for i, todo in enumerate(todos):
-            print(f"{i+1}: {todo.title()}")
-    elif todo == "exit":
-        break
+    match todo:
+        case "add":
+            todo = input("Enter a to-do: ")       
+            todos.append(todo)
+        case "show":
+            print("To-do list:")
+            for i, todo in enumerate(todos):
+                print(f"{i+1}: {todo.title()}")
+        case "edit":
+            index = int(input("Enter the number of the to-do item you want to edit: "))
+            todo = input("Enter the new to-do: ")
+            todos[index-1] = todo
+        case "exit":
+            print("Goodbye!")
+            break
+        case _:
+            print("Invalid input")
+
+
+
 
 
