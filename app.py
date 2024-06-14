467ac4931f2c0d027f6aae1dc6095ce01b4c460c
iwindon <ivan.windon@icloud.com>
2024-06-14 15:26:53 -0400

    added ability to add, edit and complete a task on the same command

--- app.py
+++ app.py
@@ -1,47 +1,48 @@
-# todos = []
+print("Todo App")
+print("To add a new to-do, type 'add' followed by the to-do.")
+print("To complete a to-do, type 'complete' followed by the to-do number.")
 
 while True:
     todo = input("Enter 'add', 'complete', 'show', 'edit' or 'exit': ")
     todo = todo.strip().lower()
 
-    match todo:
-        case "add":
-            todo = input("Enter a to-do: ") + "\n"
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
+    if "add" in todo:
+        todo = todo[4:]
+        with open("files/todos.txt", "r") as file:
+            todos = file.readlines()
 
-            todos.append(todo)
-            
-            with open("files/todos.txt", "w") as file:
-                file.writelines(todos)
-        case "show":
-            print("To-do list:")
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
-            for i, todo in enumerate(todos):
-                print(f"{i+1}: {todo.strip().title()}")
-        case "edit":
-            index = int(input("Enter the number of the to-do item you want to edit: "))
-            todo = input("Enter the new to-do: ")
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
-            todos[index-1] = todo + "\n"
-            with open("files/todos.txt", "w") as file:
-                file.writelines(todos)
-        case "complete":
-            index = int(input("Enter the number of the to-do item you want to complete: "))
-            with open("files/todos.txt", "r") as file:
-                todos = file.readlines()
-            todo_to_complete = todos[index-1]   
-            todos.pop(index-1)
-            with open("files/todos.txt", "w") as file:
-                file.writelines(todos)
-            print(f"Completed: {todo_to_complete.strip().title()}")
-        case "exit":
-            print("Goodbye!")
-            break
-        case _:
-            print("Invalid input")
+        todos.append(todo)
+        
+        with open("files/todos.txt", "w") as file:
+            file.writelines(todos)
+    elif "show" in todo:
+        print("To-do list:")
+        with open("files/todos.txt", "r") as file:
+            todos = file.readlines()
+        for i, todo in enumerate(todos):
+            print(f"{i+1}: {todo.strip().title()}")
+    elif "edit" in todo:
+        index = int(todo[5:])
+        todo = input("Enter the new to-do: ")
+        with open("files/todos.txt", "r") as file:
+            todos = file.readlines()
+        todos[index-1] = todo + "\n"
+        with open("files/todos.txt", "w") as file:
+            file.writelines(todos)
+    elif "complete" in todo:
+        index = int(todo[9:])
+        with open("files/todos.txt", "r") as file:
+            todos = file.readlines()
+        todo_to_complete = todos[index-1]   
+        todos.pop(index-1)
+        with open("files/todos.txt", "w") as file:
+            file.writelines(todos)
+        print(f"Completed: {todo_to_complete.strip().title()}")
+    elif "exit" in todo:
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid input. Please try again.")
 
 
 
