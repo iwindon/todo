cddca3b301a4ce053548d440316194db2c4c63a4
iwindon <ivan.windon@icloud.com>
2024-06-25 07:48:01 -0400

    added parameters to the function get_todos

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ print("Todo App")
 print("To add a new to-do, type 'add' followed by the to-do.")
 print("To complete a to-do, type 'complete' followed by the to-do number.")
 
-def get_todos():
-    with open("files/todos.txt", "r") as file_local:
+def get_todos(filepath):
+    with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
@@ -13,7 +13,7 @@ while True:
 
     if todo.startswith("add"):
         todo = todo[4:]
-        todos = get_todos()
+        todos = get_todos("files/todos.txt")
 
         todos.append(todo + "\n")
         
@@ -21,14 +21,14 @@ while True:
             file.writelines(todos)
     elif todo.startswith("show"):
         print("To-do list:")
-        todos = get_todos()
+        todos = get_todos("files/todos.txt")
         for i, todo in enumerate(todos):
             print(f"{i+1}: {todo.strip().title()}")
     elif todo.startswith("edit"):
         try:
             index = int(todo[5:])
             todo = input("Enter the new to-do: ")
-            todos = get_todos()
+            todos = get_todos("files/todos.txt")
             todos[index-1] = todo + "\n"
             with open("files/todos.txt", "w") as file:
                 file.writelines(todos)
@@ -42,7 +42,7 @@ while True:
     elif todo.startswith("complete"):
         try:
             index = int(todo[9:])
-            todos = get_todos()
+            todos = get_todos("files/todos.txt")
             todo_to_complete = todos[index-1]   
             todos.pop(index-1)
             with open("files/todos.txt", "w") as file:
